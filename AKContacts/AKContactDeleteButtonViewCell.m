5a1bdf3154cacaa8659aebdab07dc16a55272337
Adam Kornafeld <adam@kornafeld.com>
2013-04-16 22:23:12 -0400

    Added AKContactDeleteButtonViewCell and images

---
+++ AKContacts/AKContactDeleteButtonViewCell.m
@@ -0,0 +1,117 @@
+//
+//  AKContactDeleteButtonViewCell.m
+//
+//  Copyright (c) 2013 Adam Kornafeld All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//  3. The name of the author may not be used to endorse or promote products
+//  derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+//  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "AKContactDeleteButtonViewCell.h"
+#import "AKAddressBook.h"
+#import "AKContact.h"
+#import "AKContactViewController.h"
+
+@interface AKContactDeleteButtonViewCell () <UIActionSheetDelegate>
+
+@end
+
+@implementation AKContactDeleteButtonViewCell
+
+@synthesize parent;
+
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+  self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+  if (self)
+  {
+    // Initialization code
+  }
+  return self;
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+
+  [super setSelected:selected animated:animated];
+
+  // Configure the view for the selected state
+}
+
+- (void)dealloc {
+}
+
+-(void)configureCell
+{
+  [self setSelectionStyle: UITableViewCellSelectionStyleNone];
+  [self setBackgroundColor: [UIColor clearColor]];
+  [self setBackgroundView: [[UIView alloc] initWithFrame: CGRectZero]];
+
+  for (UIView *subView in [self.contentView subviews])
+  {
+    [subView removeFromSuperview];
+  }
+
+  CGRect frame = self.frame;
+  frame.origin.x = 10.f;
+  frame.size.width = frame.size.width - 20.f;
+
+  UIButton *button = [UIButton buttonWithType: UIButtonTypeCustom];
+  [button setFrame: frame];
+  [button setBackgroundColor: [UIColor clearColor]];
+  [button setAutoresizingMask: UIViewAutoresizingFlexibleWidth];
+	UIImage *image = [[UIImage imageNamed: @"ButtonDelete.png"] stretchableImageWithLeftCapWidth: 4 topCapHeight: 0];
+	[button setBackgroundImage: image forState: UIControlStateNormal];
+  [button addTarget: self action: @selector(deleteButtonTouchUpInside:) forControlEvents: UIControlEventTouchUpInside];
+
+  [self addSubview: button];
+
+  [button setTitle: NSLocalizedString(@"Delete Contact", @"") forState: UIControlStateNormal];
+  [button.titleLabel setFont: [UIFont boldSystemFontOfSize: 22.f]];
+  [button.titleLabel setShadowOffset: CGSizeMake(0.f, -1.f)];
+  [button.titleLabel setShadowColor: [UIColor grayColor]];
+}
+
+- (void)deleteButtonTouchUpInside: (id)sender
+{
+  UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle: nil
+                                                           delegate: self
+                                                  cancelButtonTitle: NSLocalizedString(@"Cancel", @"")
+                                             destructiveButtonTitle: NSLocalizedString(@"Delete Contact", @"")
+                                                  otherButtonTitles: nil];
+  [actionSheet showInView: self.parent.view];
+}
+
+#pragma mark - UIActionSheet delegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+  if (buttonIndex == actionSheet.destructiveButtonIndex)
+  {
+    [[AKAddressBook sharedInstance] removeRecordID: self.parent.contactID];
+    if ([self.parent.delegate respondsToSelector: @selector(recordDidRemoveWithContactID:)])
+      [self.parent.delegate recordDidRemoveWithContactID: self.parent.contactID];
+    [self.parent.navigationController popViewControllerAnimated: YES];
+  }
+}
+
+@end
