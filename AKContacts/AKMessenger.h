2b4a064a9684031b9d1e03533be5d2df21625854
Adam Kornafeld <adam.kornafeld@mediafriendsinc.com>
2013-04-29 13:28:11 -0400

    Added AKMessenger

---
+++ AKContacts/AKMessenger.h
@@ -0,0 +1,13 @@
+//
+//  AKMessenger.h
+//  Siphon
+//
+//  Created by Adam Kornafeld on 4/29/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AKMessenger : NSObject
+
+@end
---
+++ AKContacts/AKMessenger.m
@@ -0,0 +1,13 @@
+//
+//  AKMessenger.m
+//  Siphon
+//
+//  Created by Adam Kornafeld on 4/29/13.
+//
+//
+
+#import "AKMessenger.h"
+
+@implementation AKMessenger
+
+@end
