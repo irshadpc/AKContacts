86c9157cd7821205168d6cac6d56e6f53c4bc704
Adam Kornafeld <adam@kornafeld.com>
2013-05-25 22:50:58 -0400

    Added AKContactPickerViewController to project

---
+++ AKContacts/AKContactPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AKContactPickerViewController.h
+//  AKContacts
+//
+//  Created by Adam Kornafeld on 5/25/13.
+//  Copyright (c) 2013 Adam Kornafeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKContactPickerViewController : UIViewController
+
+@end
---
+++ AKContacts/AKContactPickerViewController.m
@@ -0,0 +1,29 @@
+//
+//  AKContactPickerViewController.m
+//  AKContacts
+//
+//  Created by Adam Kornafeld on 5/25/13.
+//  Copyright (c) 2013 Adam Kornafeld. All rights reserved.
+//
+
+#import "AKContactPickerViewController.h"
+
+@interface AKContactPickerViewController ()
+
+@end
+
+@implementation AKContactPickerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
