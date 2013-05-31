caa9ca8555be6aac66033f1a5e2da80e2ac51f17
Adam Kornafeld <adam@kornafeld.com>
2013-05-30 21:39:59 -0400

    Subscribe to AKContactPickerViewDidDismissNotification Removed unused code

--- AKContacts/AKContactsViewController.m
+++ AKContacts/AKContactsViewController.m
@@ -30,6 +30,7 @@
 #import "AKContactsProgressIndicatorView.h"
 #import "AKContactsRecordViewCell.h"
 #import "AKContact.h"
+#import "AKContactPickerViewController.h"
 #import "AKContactViewController.h"
 #import "AKAddressBook.h"
 #import "AKSource.h"
@@ -112,6 +113,8 @@ typedef NS_ENUM(NSInteger, ActionSheetButtons)
   [self.navigationItem setRightBarButtonItem: addButton];
   
   [[NSNotificationCenter defaultCenter] addObserver: self selector: @selector(reloadTableViewData) name: AKGroupPickerViewDidDismissNotification object: nil];
+  
+  [[NSNotificationCenter defaultCenter] addObserver: self selector: @selector(reloadTableViewData) name: AKContactPickerViewDidDismissNotification object: nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -401,41 +404,6 @@ typedef NS_ENUM(NSInteger, ActionSheetButtons)
   }
   return ret;
 }
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }   
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }   
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
 
 #pragma mark - Table view delegate
 
