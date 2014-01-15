ec2c2a9cf5e2397433f223db2ce23ed2cc91348d
Adam Kornafeld <adam@kornafeld.com>
2014-01-15 00:18:50 -0500

    Turn off layout extension

--- AKContacts/AKContactsViewController.m
+++ AKContacts/AKContactsViewController.m
@@ -111,12 +111,8 @@ typedef NS_ENUM(NSInteger, ActionSheetButtons)
 -(void)viewDidLoad
 {
   [super viewDidLoad];
-  
-  // Uncomment the following line to preserve selection between presentations.
-  // self.clearsSelectionOnViewWillAppear = NO;
-  
-  // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-  // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+  self.edgesForExtendedLayout = UIRectEdgeLeft | UIRectEdgeBottom | UIRectEdgeRight;
 
   [[NSNotificationCenter defaultCenter] addObserver: self selector: @selector(reloadTableViewData) name: AKGroupPickerViewDidDismissNotification object: nil];
   
