91e6be9a7e63c0cec7f6ced38fb9b38c26ad5a7c
Adam Kornafeld <adam.kornafeld@mediafriendsinc.com>
2014-03-11 12:47:00 -0400

    Implemented method -isMemberOfCharacterSet: Implemented method -stringByTrimmingLeadingCharactersInSet: Implemented property stringWithNonDigitsRemoved

---
+++ AKContacts/NSString+Additions.h
@@ -0,0 +1,18 @@
+//
+//  NSString+Additions.h
+//  AKContacts
+//
+//  Created by Adam Kornafeld on 3/10/14.
+//  Copyright (c) 2014 Adam Kornafeld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Additions)
+
+- (BOOL)isMemberOfCharacterSet:(NSCharacterSet *)characterset;
+- (NSString *)stringByTrimmingLeadingCharactersInSet:(NSCharacterSet *)characterSet;
+
+@property (readonly) NSString *stringWithNonDigitsRemoved;
+
+@end
---
+++ AKContacts/NSString+Additions.m
@@ -0,0 +1,39 @@
+//
+//  NSString+Additions.m
+//  AKContacts
+//
+//  Created by Adam Kornafeld on 3/10/14.
+//  Copyright (c) 2014 Adam Kornafeld. All rights reserved.
+//
+
+#import "NSString+Additions.h"
+
+@implementation NSString (Additions)
+
+- (BOOL)isMemberOfCharacterSet:(NSCharacterSet *)characterset
+{
+    BOOL ret = YES;
+    for (NSUInteger index = 0; index < self.length; ++index)
+    {
+        if (![characterset characterIsMember: [self characterAtIndex: index]])
+        {
+            ret = NO;
+            break;
+        }
+    }
+    return ret;
+}
+
+- (NSString *)stringByTrimmingLeadingCharactersInSet:(NSCharacterSet *)characterSet
+{
+    NSUInteger location = [self rangeOfCharacterFromSet: [characterSet invertedSet]].location;
+    return (location != NSNotFound) ? [self substringFromIndex: location] : @"";
+}
+
+- (NSString *)stringWithNonDigitsRemoved
+{
+    NSCharacterSet *nonDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    return [[self componentsSeparatedByCharactersInSet: nonDigits] componentsJoinedByString: @""];
+}
+
+@end
